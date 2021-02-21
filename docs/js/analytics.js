4e2dd59ab521483051dec92cb1ffb347ffe7d68a
Ramith Hettiarachchi <ramithuh@live.com>
2021-02-21 16:22:30 +0530

    analytics

--- docs/js/analytics.js
+++ docs/js/analytics.js
@@ -1,8 +1,1 @@
-// replace with our analytics
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-// ga('create', 'UA-99302615-1', 'auto');
-// ga('send', 'pageview');
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "7fdadbcd532f4e93ade3caa6bad5ca60"}'></script>
